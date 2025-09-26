3e6bba4a9573b27872153ba3991b4bacd3405e72
medotnull <meenalbansodbmw@gmail.com>
2025-09-26 23:48:39 +0530

    registering a user

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    // absolute path to your app root
+    root: ".",
+  },
 };
 
 export default nextConfig;
